d1d1821aaf7c1adbf4cf3c58c6ab8fcf4d5df480
nicomanunta <nic.manun@gmail.com>
2023-11-17 13:04:23 +0100

    esercizio 2c e 3

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let number = 100
 console.log(number);
 
-const PI = 3.1416;
+const PI = 3.14;
 console.log(PI)
+// PI = 3.1416;
+// console.log(PI)
+
+let radius = 8;
+
+let circle= radius * 2 * PI
+console.log(circle)
+
+
+let name = "Carlo";
+name = "Marco";
+console.log(name)
