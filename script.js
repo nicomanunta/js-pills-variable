08d6f4b3dab30faed97b314a84a193c7fa16d95a
nicomanunta <nic.manun@gmail.com>
2023-11-20 10:25:00 +0100

    esercizio 4 4b e 4c

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ console.log(circle)
 let name = "Carlo";
 name = "Marco";
 console.log(name)
+
+// esercizo 4
+let greet = "Ciao, Mondo!"
+console.log(greet)
+
+// esercizio 4b
+// let greet = "Come stai, mondo?"
+// console.log(greet)
+
+
+// esercizio 4c
+
+greet = greet+"Come stai?"
+greet = `${greet}Come stai?`
+console.log(totale) 
