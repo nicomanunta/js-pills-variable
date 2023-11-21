d48afcbb2fdca27e2a249401b93b19b5956da152
nicomanunta <nic.manun@gmail.com>
2023-11-21 10:30:44 +0100

    esercizio 2

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
-let number = 100
-console.log(number);
+// let number = 100
+// console.log(number);
 
-const PI = 3.14;
-console.log(PI)
-// PI = 3.1416;
+// const PI = 3.14;
 // console.log(PI)
+// // PI = 3.1416;
+// // console.log(PI)
 
-let radius = 8;
+// let radius = 8;
 
-let circle= radius * 2 * PI
-console.log(circle)
+// let circle= radius * 2 * PI
+// console.log(circle)
 
 
-let name = "Carlo";
-name = "Marco";
-console.log(name)
+// let name = "Carlo";
+// name = "Marco";
+// console.log(name)
 
-// esercizo 4
-let greet = "Ciao, Mondo!"
-console.log(greet)
-
-// esercizio 4b
-// let greet = "Come stai, mondo?"
+// // esercizo 4
+// let greet = "Ciao, Mondo!"
 // console.log(greet)
 
-
-// esercizio 4c
-
-greet = greet+"Come stai?"
-greet = `${greet}Come stai?`
-console.log(totale) 
+// // esercizio 4b
+// // let greet = "Come stai, mondo?"
+// // console.log(greet)
 
 
-// esercizio 1
+// // esercizio 4c
 
-let num1 = 40
-let num2 = 30
+// greet = greet+"Come stai?"
+// greet = `${greet}Come stai?`
+// console.log(totale) 
 
-let comparison;
 
-if (num1 > num2){
-    comparisono= num1
+// // esercizio 1
+
+// let num1 = 40
+// let num2 = 30
+
+// let comparison;
+
+// if (num1 > num2){
+//     comparisono= num1
+// }
+// else {
+//     comaparison = num2
+// }
+// console.log(comparison)
+
+
+// esercizio 2
+let nome1 = "carlo"
+let nome2 = "giorgio"
+let comparison 
+
+if (nome1 == nome2){
+    comparison = "vero"
 }
 else {
-    comaparison = num2
+    comparison = "falso"
 }
 console.log(comparison)
